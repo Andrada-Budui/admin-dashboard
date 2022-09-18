34fa89d3644e3e9db097357866c3b9a25fe6da15
Andrada Budui <andrada.budui@yahoo.com>
2022-09-18 19:33:21 +0300

    finished sidebar and navbar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,16 @@
 import "./sidebar.scss";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
+import SettingsSystemDaydreamIcon from "@mui/icons-material/SettingsSystemDaydream";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Sidebar = () => {
     return (
@@ -10,22 +21,65 @@ const Sidebar = () => {
             <hr />
             <div className="center">
                 <ul>
+                    <p className="title">MAIN</p>
                     <li>
-                        <DashboardIcon />
+                        <DashboardIcon className="icon" />
                         <span>Dashboard</span>
                     </li>
+                    <p className="title">LISTS</p>
                     <li>
-                        <span>Dashboard</span>
+                        <PersonOutlineIcon className="icon" />
+                        <span>Users</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <StoreIcon className="icon" />
+                        <span>Products</span>
                     </li>
                     <li>
-                        <span>Dashboard</span>
+                        <CreditCardIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon" />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <BarChartIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsActiveIcon className="icon" />
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyOutlinedIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountBoxIcon className="icon" />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon className="icon" />
+                        <span>Logout</span>
                     </li>
                 </ul>
             </div>
-            <div className="bottom"> color options</div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
         </div>
     );
 };
