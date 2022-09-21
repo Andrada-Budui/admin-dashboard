e7f2882142a894514748f7a39ff2a20d134da5ce
Andrada Budui <andrada.budui@yahoo.com>
2022-09-21 20:11:20 +0300

    finished homepage

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,7 +1,102 @@
 import "./widget.scss";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
 
-const Widget = () => {
-    return <div className="widget">Widget</div>;
+const Widget = ({ type }) => {
+    let data;
+
+    // static data (temporary)
+    const amount = 100;
+    const diff = 20;
+    switch (type) {
+        case "user":
+            data = {
+                title: "USERS",
+                isMoney: false,
+                link: "See all users",
+                icon: (
+                    <PersonOutlineOutlinedIcon
+                        className="icon"
+                        style={{
+                            color: "crimson",
+                            backgroundColor: "rgba(255,0,0,0.2)",
+                        }}
+                    />
+                ),
+            };
+            break;
+        case "order":
+            data = {
+                title: "ORDERS",
+                isMoney: false,
+                link: "See all orders",
+                icon: (
+                    <ShoppingCartOutlinedIcon
+                        className="icon"
+                        style={{
+                            backgroundColor: "rgba(218,165,32,0.2)",
+                            color: "goldenrod",
+                        }}
+                    />
+                ),
+            };
+            break;
+        case "earning":
+            data = {
+                title: "EARNINGS",
+                isMoney: true,
+                link: "View net earnings",
+                icon: (
+                    <MonetizationOnOutlinedIcon
+                        className="icon"
+                        style={{
+                            backgroundColor: "rgba(0,128,0,0.2)",
+                            color: "green",
+                        }}
+                    />
+                ),
+            };
+            break;
+        case "balance":
+            data = {
+                title: "BALANCE",
+                isMoney: true,
+                link: "See details",
+                icon: (
+                    <AccountBalanceOutlinedIcon
+                        className="icon"
+                        style={{
+                            backgroundColor: "rgba(128,0,128,0.2)",
+                            color: "purple",
+                        }}
+                    />
+                ),
+            };
+            break;
+        default:
+            break;
+    }
+    return (
+        <div className="widget">
+            <div className="left">
+                <div className="title">{data.title}</div>
+                <div className="counter">
+                    {data.isMoney && "$"} {amount}
+                </div>
+                <div className="link">{data.link}</div>
+            </div>
+            <div className="right">
+                <div className="percentage positive">
+                    <KeyboardArrowUpOutlinedIcon />
+                    {diff}%
+                </div>
+                {data.icon}
+            </div>
+        </div>
+    );
 };
 
 export default Widget;
