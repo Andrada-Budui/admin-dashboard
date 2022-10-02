693a02810c2f02fb4533c835c771d5a4548101a9
Andrada Budui <andrada.budui@yahoo.com>
2022-10-02 17:59:54 +0300

    add new form, links and darkmode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,8 +7,12 @@ import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNone
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 import avatar from "../../img/pexels-luizclas-595747.jpg";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Navbar = () => {
+    const { dispatch } = useContext(DarkModeContext);
+
     return (
         <div className="navbar">
             <div className="wrapper">
@@ -22,7 +26,10 @@ const Navbar = () => {
                         English
                     </div>
                     <div className="item">
-                        <DarkModeOutlinedIcon className="icon" />
+                        <DarkModeOutlinedIcon
+                            className="icon"
+                            onClick={() => dispatch({ type: "TOGGLE" })}
+                        />
                     </div>
                     <div className="item">
                         <FullscreenExitOutlinedIcon className="icon" />
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,12 +11,19 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import SettingsIcon from "@mui/icons-material/Settings";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../../context/darkModeContext";
 
 const Sidebar = () => {
+    const { dispatch } = useContext(DarkModeContext);
+
     return (
         <div className="sidebar">
             <div className="top">
-                <span className="logo">andradmin</span>
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">andradmin</span>
+                </Link>
             </div>
             <hr />
             <div className="center">
@@ -27,14 +34,18 @@ const Sidebar = () => {
                         <span>Dashboard</span>
                     </li>
                     <p className="title">LISTS</p>
-                    <li>
-                        <PersonOutlineIcon className="icon" />
-                        <span>Users</span>
-                    </li>
-                    <li>
-                        <StoreIcon className="icon" />
-                        <span>Products</span>
-                    </li>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PersonOutlineIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <StoreIcon className="icon" />
+                            <span>Products</span>
+                        </li>
+                    </Link>
                     <li>
                         <CreditCardIcon className="icon" />
                         <span>Orders</span>
@@ -77,8 +88,14 @@ const Sidebar = () => {
                 </ul>
             </div>
             <div className="bottom">
-                <div className="colorOption"></div>
-                <div className="colorOption"></div>
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({ type: "LIGHT" })}
+                ></div>
+                <div
+                    className="colorOption"
+                    onClick={() => dispatch({ type: "DARK" })}
+                ></div>
             </div>
         </div>
     );
